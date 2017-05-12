ae9722db1cedc70e6fc88475a57d9f23d458d767
arunpallikandath <arunpallikandath@gmail.com>
2017-05-12 07:01:21 -0500

    latest changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Http } from '@angular/http';
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { TabsPage } from '../pages/tabs/tabs';
 import { HomePage } from '../pages/home/home';
+import { Events } from 'ionic-angular';
 
 @Component({
   templateUrl: 'app.html'
@@ -13,8 +12,9 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
   private loginrs:any;
-
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private http: Http) {
+  
+  constructor(platform: Platform, statusBar: StatusBar, 
+  splashScreen: SplashScreen, private http: Http, private events: Events) {
 
     
     platform.ready().then(() => {
@@ -31,6 +31,13 @@ export class MyApp {
     
     });
 
+    //
 
   }
+  //
+  loadContent(loginrsItem){
+     
+    this.events.publish ('content-update', loginrsItem) 
+  }
+  //
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Http} from '@angular/http';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Events } from 'ionic-angular';
 import 'rxjs/add/operator/map';
 
 @Component({
@@ -9,11 +9,17 @@ import 'rxjs/add/operator/map';
 })
 export class HomePage {
 
-   private loginrs: any;
-  constructor(public navCtrl: NavController, private http: Http) {
 
+  private loginDetails: any;
+  constructor(public navCtrl: NavController, private http: Http, private events: Events ) {
+    //
 
+    this.events.subscribe('content-update', (params) => this.updateLoginRs(params))
   }
 
+  updateLoginRs(loginRs:any){
+
+    this.loginDetails = loginRs
+  }
 
 }
